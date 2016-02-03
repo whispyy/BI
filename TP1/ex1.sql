80c2105f0ba6c50ef5c4ad088dc53add76e3667c
whispyy <jfdurk@hotmail.fr>
2016-02-03 13:42:47 +0100

    TP1 premiere partie reformulé

--- TP1/ex1.sql
+++ TP1/ex1.sql
@@ -1,42 +1,33 @@
-DROP TABLE DEPT;
-DROP TABLE EMP;
+-- ex1
 
-CREATE TABLE DEPT
-    (
-    DEPTNO NUMERIC(2),
-    DNAME VARCHAR(14),
-    LOC VARCHAR(13),
-    PRIMARY KEY("DEPTNO")
-    );
 
-CREATE TABLE EMP
-    (
-    EMPNO NUMERIC(4),
-    ENAME VARCHAR(10),
-    JOB VARCHAR(9),
-    MGR NUMERIC(4),
-    HIREDATE DATE,
-    SAL NUMERIC(7,2),
-    COMM NUMERIC(7,2),
-    DEPTNO NUMERIC(2)
-    );
+-- 1.a
+select DEPTNO, ENAME, SAL, RANK() 
+    OVER (partition by DEPTNO order by DEPTNO, SAL DESC) as RANG 
+    from EMP 
+        where DEPTNO = 10 or DEPTNO = 30;
 
-INSERT INTO DEPT VALUES(10,'ACCOUNTING','NEW YORK');
-INSERT INTO DEPT VALUES(20,'RESEARCH','DALLAS');
-INSERT INTO DEPT VALUES(30,'SALES','CHICAGO');
-INSERT INTO DEPT VALUES(40,'OPERATIONS','BOSTON');
-INSERT INTO EMP VALUES(7369,'SMITH','CLERK',7902,to_date('17-12-1980','dd-mm-yyyy'),800,NULL,20);
-INSERT INTO EMP VALUES(7499,'ALLEN','SALESMAN',7698,to_date('20-2-1981','dd-mm-yyyy'),1600,300,30);
-INSERT INTO EMP VALUES(7521,'WARD','SALESMAN',7698,to_date('22-2-1981','dd-mm-yyyy'),1250,500,30);
-INSERT INTO EMP VALUES(7566,'JONES','MANAGER',7839,to_date('2-4-1981','dd-mm-yyyy'),2975,NULL,20);
-INSERT INTO EMP VALUES(7654,'MARTIN','SALESMAN',7698,to_date('28-9-1981','dd-mm-yyyy'),1250,1400,30);
-INSERT INTO EMP VALUES(7698,'BLAKE','MANAGER',7839,to_date('1-5-1981','dd-mm-yyyy'),2850,NULL,30);
-INSERT INTO EMP VALUES(7782,'CLARK','MANAGER',7839,to_date('9-6-1981','dd-mm-yyyy'),2450,NULL,10);
-INSERT INTO EMP VALUES(7788,'SCOTT','ANALYST',7566,to_date('13-7-87','dd-mm-yy')-85,3000,NULL,20);
-INSERT INTO EMP VALUES(7839,'KING','PRESIDENT',NULL,to_date('17-11-1981','dd-mm-yyyy'),5000,NULL,10);
-INSERT INTO EMP VALUES(7844,'TURNER','SALESMAN',7698,to_date('8-9-1981','dd-mm-yyyy'),1500,0,30);
-INSERT INTO EMP VALUES(7876,'ADAMS','CLERK',7788,to_date('13-7-87', 'dd-mm-yy')-51,1100,NULL,20);
-INSERT INTO EMP VALUES(7900,'JAMES','CLERK',7698,to_date('3-12-1981','dd-mm-yyyy'),950,NULL,30);
-INSERT INTO EMP VALUES(7902,'FORD','ANALYST',7566,to_date('3-12-1981','dd-mm-yyyy'),3000,NULL,20);
-INSERT INTO EMP VALUES(7934,'MILLER','CLERK',7782,to_date('23-1-1982','dd-mm-yyyy'),1300,NULL,10);
-COMMIT;
+-- 1.b
+select DEPTNO, ENAME, SAL, DENSE_RANK() OVER (partition by DEPTNO order by DEPTNO, SAL DESC) as RANG from EMP where DEPTNO = 10 or DEPTNO = 30;
+
+-- 1.c
+select unique DEPTNO, SAL, DENSE_RANK() OVER (partition by DEPTNO order by DEPTNO, SAL DESC) as RANG from EMP where DEPTNO = 10 or DEPTNO = 20 order by DEPTNO, SAL DESC;
+
+-- 1.d (1)
+select JOB, sum(SAL) as TOTAL_SAL_JOB from EMP group by JOB;
+
+-- 1.d(2)
+select distinct JOB, sum(SAL) over (partition by JOB) as TOTAL_SAL_JOB from EMP;
+
+-- 1.f
+select deptno, job, sum(sal) from EMP
+    group by 
+        grouping sets ((deptno),(deptno, job),());
+
+-- 1.g (1)
+select decode(deptno, null, 'TousDep', deptno, deptno), decode (job, null, 'TousEmployes', job, job), sum(sal) from EMP
+group by grouping sets ((deptno),(deptno, job),());
+
+-- 1.g (2)
+select decode(deptno, null, 'TousDep', deptno, deptno), nvl(job,'TousEmployes'), sum(sal) from EMP
+group by grouping sets ((deptno),(deptno, job),());
